b8cc78988697dd9d0ed4b9eef4874a765fe6d154
elisehodbert <elisehodbert21@gmail.com>
2026-05-29 13:47:03 +0200

    update with included and excluded records

--- temp_reason_for_exclusion.R
+++
@@ -1,7 +0,0 @@
-excluded <- read.csv("excluded.csv")
-reason = excluded$Notes
-reason_clean <- sub(";.*", "", sub(".*Exclusion reason: ", "", reason))
-writeClipboard(paste(reason_clean, collapse = "\n"))
-
-
-irrelevant <- read.csv("irrelevant.csv")
